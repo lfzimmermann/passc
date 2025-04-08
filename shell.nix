d2ab537fff69026d1b9d1206a3f3e494e1b40602
Luca-Frank Zimmermann <luca-frank.zimmermann@stud.uni-bamberg.de>
2025-04-08 19:09:00 +0200

    added gitignore to ignore result link; cleaned up nix-shell environment;

--- shell.nix
+++ shell.nix
@@ -3,22 +3,8 @@ let
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 pkgs.mkShell {
-  packages = with pkgs; [
-		ncurses
-		pkg-config
-		prettierd
-		clang
-		gcc
-		libclang
-		libcxx
-		ninja
-		meson
-  ];
   shellHook = ''
-		nix-build
 		PROJDIR=$(pwd)
 		PATH=$PROJDIR/result/bin/:$PATH
-		passc &&
-		exit
   '';
 }
